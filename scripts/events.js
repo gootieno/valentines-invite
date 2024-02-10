e57a4e1019d7c8cb4ac29a088fe07e026602257e
gootieno <geoff.otieno7@gmail.com>
2024-02-09 20:10:58 -0800

    youtube working

--- scripts/events.js
+++ scripts/events.js
@@ -1,32 +1,6 @@
 // import player from "./music.js";
 let rainInterval;
-
-var player;
-export function onYouTubeIframeAPIReady() {
-  player = new YT.Player("player", {
-    height: "300px", // Set to '0' to hide the video; '1' for minimal visibility
-    width: "400px",
-    videoId: "xqfckiR2lfs", // Your YouTube video ID
-    playerVars: {
-      autoplay: 1, // Don't autoplay on load
-      controls: 0, // No controls
-      showinfo: 0,
-      modestbranding: 1,
-      loop: 1,
-      fs: 0,
-      cc_load_policy: 0,
-      iv_load_policy: 3,
-      autohide: 0,
-    },
-    events: {
-      onReady: onPlayerReady,
-    },
-  });
-}
-
-function onPlayerReady(event) {
-  // Player is ready
-}
+import { player } from "./index.js";
 
 export const handleShowCat = () => {
   const yesButton = document.getElementById("yes");
--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,33 @@
 import { createCardImageContainer } from "./card.js";
 import { handleShowCat } from "./events.js";
 
+export var player;
+window.onYouTubeIframeAPIReady = function () {
+  player = new YT.Player("player", {
+    height: "300px", // Set to '0' to hide the video; '1' for minimal visibility
+    width: "400px",
+    videoId: "xqfckiR2lfs", // Your YouTube video ID
+    playerVars: {
+      autoplay: 0, // Don't autoplay on load
+      controls: 0, // No controls
+      showinfo: 0,
+      modestbranding: 1,
+      loop: 1,
+      fs: 0,
+      cc_load_policy: 0,
+      iv_load_policy: 3,
+      autohide: 0,
+    },
+    events: {
+      onReady: onPlayerReady,
+    },
+  });
+};
+
+function onPlayerReady(event) {
+  // Player is ready
+}
+
 const createBackgroundImage = () => {
   const backgroundImage = document.createElement("img");
   backgroundImage.setAttribute("id", "valentines-background-image");
@@ -10,11 +37,6 @@ const createBackgroundImage = () => {
 };
 
 const initializePage = () => {
-  let tag = document.createElement("script");
-  tag.src = "https://www.youtube.com/iframe_api";
-  let firstScriptTag = document.getElementsByTagName("script")[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
   createBackgroundImage();
 
   const mainContainer = document.createElement("div");
@@ -28,4 +50,11 @@ const initializePage = () => {
   handleShowCat();
 };
 
-window.onload = initializePage;
+window.onload = () => {
+  let tag = document.createElement("script");
+  tag.src = "https://www.youtube.com/iframe_api";
+  let firstScriptTag = document.getElementsByTagName("script")[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  initializePage();
+};
